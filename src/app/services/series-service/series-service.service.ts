68af52dedcc7fa19ab5c43ab5800f7469a6117b7
lexash707 <lexashart@gmail.com>
2024-06-22 12:28:22 +0200

    safety commit

--- src/app/services/series-service/series-service.service.ts
+++ src/app/services/series-service/series-service.service.ts
@@ -14,6 +14,6 @@ export class SeriesServiceService {
 
   getAll():Observable<Movie[]> {
     return this.httpClient.get<any>("http://localhost:8080/series/all")
-    .pipe(map(data => data.map((item: { naziv: string; slika: string; }) => new Movie(item.naziv, item.slika))));
+    .pipe(map(data => data.map((item: { naziv: string; slika: string; idSerije: number; }) => new Movie(item.naziv, item.slika, item.idSerije))));
   }
 }
